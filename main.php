f4b4da490f1c5f6d07c819b6e8355254daa20f43
SMSLink.ro <contact@smslink.ro>
2020-08-14 06:56:34 +0000

    Initial commit

---
+++ main.php
@@ -0,0 +1,28 @@
+<?php
+
+$client = new http\Client;
+$request = new http\Client\Request;
+
+$request->setRequestUrl('https://secure.smslink.ro/sms/gateway/communicate/index.php');
+$request->setRequestMethod('GET');
+
+/* 
+
+  Get your SMSLink / SMS Gateway Connection ID and Password from 
+  https://www.smslink.ro/get-api-key/
+
+*/
+
+$request->setQuery(new http\QueryString(array(
+  'connection_id' => '... My SMS Gateway Connection ID ...',
+	'password' => '... My SMS Gateway Password ...',
+	'to' => '07xyzzzzzz',
+	'message' => 'My Test Message'
+)));
+
+$client->enqueue($request)->send();
+$response = $client->getResponse();
+
+echo $response->getBody();
+
+?>
